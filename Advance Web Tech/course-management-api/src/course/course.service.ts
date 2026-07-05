22119717de35c804c31936760372986f35866f21
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-07-05 13:47:35 +0600

    Complete lab task - 1

--- Advance Web Tech/course-management-api/src/course/course.service.ts
+++ Advance Web Tech/course-management-api/src/course/course.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  getAllCourses(): string {
+    return 'Get All Courses';
+  }
+
+  getCourseById(id: string): string {
+    return `Get Course by ID: ${id}`;
+  }
+
+  createCourse(): string {
+    return 'Create Course';
+  }
+
+  updateCourse(id: string): string {
+    return `Update Course ${id}`;
+  }
+
+  patchCourse(id: string): string {
+    return `Patch Course ${id}`;
+  }
+
+  deleteCourse(id: string): string {
+    return `Delete Course ${id}`;
+  }
+}
