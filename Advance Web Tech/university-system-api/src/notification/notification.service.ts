a72083d7d4caa39907d57fc691e23736e9b09d3c
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-07-13 03:51:38 +0600

    Complete lab task 3

---
+++ Advance Web Tech/university-system-api/src/notification/notification.service.ts
@@ -0,0 +1,27 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { EnrollmentService } from '../enrollment/enrollment.service';
+
+@Injectable()
+export class NotificationService {
+  constructor(
+    @Inject(forwardRef(() => EnrollmentService))
+    private readonly enrollmentService: EnrollmentService,
+  ) {}
+
+  sendNotification(studentName: string, message: string) {
+    return {
+      message: 'Notification sent',
+      student: studentName,
+      text: message,
+    };
+  }
+
+  checkEnrollmentAndNotify(studentName: string, courseId: string) {
+    const enrollments = this.enrollmentService.getEnrollments();
+
+    return {
+      message: `Enrollment checked for ${studentName} in course ${courseId}`,
+      enrollments,
+    };
+  }
+}
